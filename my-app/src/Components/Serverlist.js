6a048efaecd16f73ab3432faa6fa1d7120e34736
ddelfaus <ddelfaus@gmail.com>
2019-11-07 16:31:07 -0500

    first commit and basic layout

---
+++ my-app/src/Components/Serverlist.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect} from 'react-redux'
+
+const Serverlist = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+const mapStateToProps = state => {
+    return(
+        <div>
+            
+        </div>
+    )
+
+
+}
+
+export default connect(mapSateToProps, [])(Serverlist);
